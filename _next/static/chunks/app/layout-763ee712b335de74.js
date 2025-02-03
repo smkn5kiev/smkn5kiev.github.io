257f4f670598f367c3ff9e2a40f273b1ab238324
Vendrika <vendrika105@gmail.com>
2025-02-03 15:34:25 +0700

    Initial Commit

---
+++ _next/static/chunks/app/layout-763ee712b335de74.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{4495:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,8173,23)),Promise.resolve().then(n.t.bind(n,5786,23)),Promise.resolve().then(n.t.bind(n,4051,23))},4051:()=>{},5786:()=>{}},e=>{var s=s=>e(e.s=s);e.O(0,[778,173,441,517,358],()=>s(4495)),_N_E=e.O()}]);
